a97762845b45437d2890f4613823e63893c389c6
Diego Orellana <a2024114415@estudiantes.upsa.edu.bo>
2025-08-20 19:52:05 -0400

    Crear CalculatorController.cs

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Calculator_API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+    }
+}
